06baf62bffe85d15e72359612b5954358e9e778c
huytq <huytranquang.150701@gmail.com>
2025-07-24 23:28:58 +0700

    merge main

--- src/components/Layout/BaseLayout.tsx
+++ src/components/Layout/BaseLayout.tsx
@@ -4,13 +4,24 @@ import FloatingContacts from './FloatingContacts'
 import Navbar from './Navbar'
 import FreeTestFiles from '../Home/FreeTestFiles'
 import Footer from './Footer'
+import { useRef } from 'react'
 
 export default function BaseLayout({ children }: { children: React.ReactNode }) {
+  const contactRef = useRef<HTMLDivElement>(null)
+
+  const scrollToContact = () => {
+    if (contactRef.current) {
+      contactRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
   return (
     <>
-      <Navbar />
+      <Navbar onSendFreeTest={scrollToContact} />
       {children}
-      <FreeTestFiles />
+      <div ref={contactRef}>
+        <FreeTestFiles />
+      </div>
       <FloatingContacts />
       <Footer />
     </>
